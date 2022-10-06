063da2a35f9162ddc61b9fa33213fc301192c6d1
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-06 21:57:45 +0300

    Update Cast.jsx

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,1 +1,15 @@
-export const Cast = () => { };
+const Cast = () => { 
+    return (
+        <div>
+            <ul>
+                <li>
+                    <img src="" alt="actor"></img>
+                    <p>Name</p>
+                    <p>Character:</p>
+                </li>
+            </ul>
+        </div>
+    )
+};
+
+export default Cast;
