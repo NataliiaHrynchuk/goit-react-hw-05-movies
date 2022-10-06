2a1c8c35099ead453ca9948599da7b145ff906e6
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-06 21:57:51 +0300

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,1 +1,12 @@
-export const Reviews = () => { };
+const Reviews = () => {
+    return (
+        <div>
+            <h3>
+                Author
+            </h3>
+            <p>Text</p>
+        </div>
+    )
+};
+ 
+export default Reviews;
