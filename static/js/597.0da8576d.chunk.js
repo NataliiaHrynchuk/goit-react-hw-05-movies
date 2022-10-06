47788fa207820a5cf6f389e9f0364b661a47f076
NataliiaHrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-06 19:01:21 +0000

    Deploying to gh-pages from @ NataliiaHrynchuk/goit-react-hw-05-movies@8ead2ddbf7aebad801d3ab5da6b4ac379f08057e 🚀

---
+++ static/js/597.0da8576d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[597],{3597:function(e,r,c){c.r(r);var i=c(184);r.default=function(){return(0,i.jsx)("div",{children:(0,i.jsx)("ul",{children:(0,i.jsxs)("li",{children:[(0,i.jsx)("img",{src:"",alt:"actor"}),(0,i.jsx)("p",{children:"Name"}),(0,i.jsx)("p",{children:"Character:"})]})})})}}}]);
+//# sourceMappingURL=597.0da8576d.chunk.js.map
