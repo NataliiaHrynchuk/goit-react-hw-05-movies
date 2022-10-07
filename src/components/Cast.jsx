cf9d0486b604ced89c388cd2d956b8dc4041cb64
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-07 16:15:14 +0300

    Update Cast.jsx

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,12 +1,34 @@
+import * as API from '../services/api';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
 const Cast = () => { 
+    const { movieId } = useParams();
+    const [cast, setCast] = useState([]);
+
+    useEffect(() => {
+        if (!movieId) {
+            return;
+        }
+        API
+            .getCast(movieId)
+            .then(({ cast }) => {
+                setCast([...cast]);
+                console.log(cast);
+        })
+    }, [movieId])
     return (
         <div>
             <ul>
-                <li>
-                    <img src="" alt="actor"></img>
-                    <p>Name</p>
-                    <p>Character:</p>
-                </li>
+                {cast.map(({ character, name, profile_path, id }) => 
+                    profile_path &&
+                    <li key={id}>
+                        <img src={`https://image.tmdb.org/t/p/w200${profile_path}`} alt="actor"></img>
+                        <p>{name}</p>
+                        {character && <p>Character: {character }</p>}
+                    </li>
+                )}
+                
             </ul>
         </div>
     )
