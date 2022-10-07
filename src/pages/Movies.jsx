cd1eb7cb03ac620de6b4826b4bed7210aad33c32
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-07 14:00:15 +0300

    add useSearchParams

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,24 +1,25 @@
 import SearchForm from '../components/SearchForm/SearchForm';
 import { useEffect, useState } from 'react';
 import { MovieList } from 'components/MovieList';
+import { useSearchParams } from 'react-router-dom';
 import * as API from '../services/api';
 
 const Movies = () => {
-    const [movieName, setMovieName] = useState('');
     const [movies, setMovies] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get('query') ?? "";
+    console.log(query);
     
-    const handleFormSubmit = query => { 
-        setMovieName(query);
-        setMovies([]);
-        
+    const handleFormSubmit = name => { 
+        setSearchParams(name !== "" ? { query: name } : {});        
     };
 
     useEffect(() => {
-        if (!movieName) {
+        if (!query) {
             return;
         }
         API
-            .getMoviesByName(movieName)
+            .getMoviesByName(query)
             .then((data) => {
                 const receivedMovies = data.results.map(
                     ({ id,
@@ -33,9 +34,9 @@ const Movies = () => {
                     }
                 );
                 console.log(receivedMovies);
-                setMovies(prevMovies => [...prevMovies, ...receivedMovies]);
+                setMovies([...receivedMovies]);
             })
-    }, [movieName]);
+    }, [query]);
     return (
         <main>
             <SearchForm onSubmit={handleFormSubmit} />
