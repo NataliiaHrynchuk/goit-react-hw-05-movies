3234e6999b0debc697f53c268b16e2ff61194c41
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-07 16:15:27 +0300

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,37 +1,25 @@
 import React from "react";
-import { useState } from "react";
 import * as SC from './SearchForm.style';
 
-
-
-
-export default function SearchForm({ onSubmit }) {
-    const [query, setQuery] = useState('');
-                
-    const handleNameChange = event => {
-    setQuery(event.currentTarget.value.toLowerCase());
-    };
-        
+export default function SearchForm({ onSubmit }) {            
     const handleSubmit = event => {
-    event.preventDefault();
-    if (query === ' ') {
-        alert('Enter movies name');
+        event.preventDefault();
+        
+        if (!event.currentTarget.elements.query.value) {
+            alert('Enter movies name');
         return;
-    }
-    onSubmit(query);
-    setQuery(' ');
+        }
+        onSubmit(event.currentTarget.elements.query.value);
+        event.currentTarget.reset();
     };
 
     return (
         <SC.SearchForm onSubmit={handleSubmit}>
             <SC.SearchFormInput
                 type="text"
-                name="imagesName"
-                value={query}
-                onChange={handleNameChange}
+                name="query"
                 autoFocus
                 utocomplete="off"
-                placeholder=""
             />
             <SC.SearchFormBtn type="submit">
                 Search
