2bf00ea3b14683c67a73b48a3f5589d97c52f407
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-11 21:35:38 +0300

    Update Cast.jsx

--- src/components/MoreDetails/Cast.jsx
+++ src/components/MoreDetails/Cast.jsx
@@ -1,6 +1,9 @@
 import * as API from '../../services/api';
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import defaultPhoto from 'components/images/free-sticker-think-7598091.png'
+
+const IMG_URL = 'https://image.tmdb.org/t/p/w200';
 
 const Cast = () => { 
     const { movieId } = useParams();
@@ -9,14 +12,22 @@ const Cast = () => {
     useEffect(() => {
         API
             .getCast(movieId)
-            .then(setCast)
+            .then(cast => 
+                setCast(cast.map(({ id, name, character, profile_path: photo}) => ({
+                    id,
+                    name, 
+                    character,
+                    photo: photo ? IMG_URL + photo : defaultPhoto,
+                }))
+            ))
+                
     }, [movieId]);
 
     return (
         <div>
             {cast.length > 0 && (
                 <ul>
-                {cast.map(({ character, name, photo, id }) => {
+                {cast.map(({id, name, character, photo }) => {
                     return (
                         <li key={id}>
                         <img src={photo} alt="actor"></img>
