384768858ee4e1069dd33b71c6b633a337fb8752
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-11 21:35:49 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,18 +5,17 @@ import { Suspense } from 'react';
 import * as API from '../services/api';
 import * as SC from './MovieDetails.styled';
 
-const Status = {
-    IDLE: 'idle',
-    RESOLVED: 'resolved',
-    REJECTED: 'rejected',
-}
+
+const dafaultURL = 'https://i.ibb.co/BrYLsTv/default-movie-poster-min.jpg';
+
 export default function MovieDetails() { 
     const { movieId } = useParams();
-    const [movie, setMovie] = useState('');
-    const [status, setStatus] = useState('idle');
-    const [error, setError] = useState(null);
+    const [movie, setMovie] = useState(null);
     const location = useLocation();
     const backLinkHref = location?.state?.from ?? "/movies";
+
+    const { poster, title, name, releaseYear, userScore, overview, genres } =
+    movie ?? {};
     
 
     useEffect(() => {
@@ -25,39 +24,31 @@ export default function MovieDetails() {
         }
         API
             .getMovieById(movieId)
-            .then((data) => {
-                console.log(data);
-                setMovie(data);
-                setStatus(Status.RESOLVED);
-            })
-            .catch(error => {
-                setError(error);
-                setStatus(Status.REJECTED);
-        })
+            .then(setMovie)
     }, [movieId]);
     
     return (
         <div>
-            {status === Status.REJECTED && <p>{error.message}</p>}
+            
         {movie &&
                 <>
-                <BackLink to={backLinkHref }>Go back</BackLink>
+                <BackLink to={backLinkHref}>Go back</BackLink>
                     <SC.Box>
-                        {movie.poster_path ? (
-                        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                        {poster ? (
+                        <img src={poster}
                             alt='movie cover' />
                         ) : (
-                            <img src = 'https://i.ibb.co/BrYLsTv/default-movie-poster-min.jpg'
+                            <img src = {dafaultURL}
                             alt='movie cover' width="200px" />    
                         )}
                     
                         <div>
-                            <h2>{movie.title || movie.name}</h2>
-                            <p>User Score: {Math.round(movie.vote_average * 10)}%</p>
+                            <h2>{title || name}({releaseYear})</h2>
+                            <p>User Score: {userScore}%</p>
                             <h3>Overview</h3>
-                            <p>{movie.overview}</p>
+                            <p>{overview}</p>
                             <h4>Genres</h4>
-                            <p>{movie.genres.map(genre => genre.name).join(', ')}</p>
+                            <p>{genres.map(genre => genre.name).join(', ')}</p>
                         </div>
                     </SC.Box>
                     <SC.Wrap>
