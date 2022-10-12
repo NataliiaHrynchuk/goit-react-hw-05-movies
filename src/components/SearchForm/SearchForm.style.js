58e8d1900d1c097128a025f1f4aebdd1c2b3aa49
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-12 19:11:05 +0300

    Update SearchForm.style.js

--- src/components/SearchForm/SearchForm.style.js
+++ src/components/SearchForm/SearchForm.style.js
@@ -2,24 +2,27 @@ import styled from "styled-components";
 
 export const SearchForm = styled.form`
     display: flex;
-    margin-left: 10px;
+    margin-left: auto;
+    margin-right: auto;
     align-items: center;
-    width: 100%;
+    width: 400px;
+    height: 32px;
     max-width: 600px;
     background-color: #fff;
-    border-radius: 3px;
+    border: solid 1px #c71585;
+    border-radius: 4px;
     overflow: hidden;
 `;
 
 export const SearchFormBtn = styled.button`
     display: inline-block;
-    height: 31px;
+    height: 100%;
     padding: 4px;
     opacity: 0.8;
     transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
     cursor: pointer;
     outline: none;
-    border: solid 1px #b7b2b2;
+    border: none;
     color: black;
     background-color: #c6bacb;
     :hover, :focus {
@@ -31,18 +34,22 @@ export const SearchFormBtn = styled.button`
 `;
 
 export const SearchFormInput = styled.input`
+box-sizing: border-box;
     display: inline-block;
     width: 100%;
     height: 100%;
     font: inherit;
-    font-size: 20px;
-    border: solid 1px #b7b2b2;
+    font-size: 16px;
+    vertical-align: middle;
+    /* border: solid 1px #c71585; */
+    border: none;
     outline: none;
-    padding-left: 4px;
-    padding-right: 4px;
-    :focus {
+    padding: 4px;
+    /* padding-left: 4px;
+    padding-right: 4px; */
+    /* :focus {
         border: solid 1px #139cd1;
-    }
+    } */
     ::placeholder {
         font: inherit;
         font-size: 18px;
