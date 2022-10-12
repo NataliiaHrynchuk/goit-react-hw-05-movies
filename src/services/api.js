b40e4cf49b17b93ee6556c30cba6a9013d0feb7e
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-12 19:11:38 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,42 +1,33 @@
 import axios from 'axios';
-const API_KEY = '6860d409037e6854532ae07af7af1d83';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const IMG_URL = 'https://image.tmdb.org/t/p/w200';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.params = {
-    api_key: '2123fea73978ac77fe6a3f347cf7ced6',
+    api_key: '6860d409037e6854532ae07af7af1d83',
     include_adult: false,
 };
 
+const IMG_URL = 'https://image.tmdb.org/t/p/w200';
 
-
-export async function getTradingMovies() {
-    try {
-        const response = await axios.get(`${BASE_URL}/trending/all/day?api_key=${API_KEY}`);
-        return response.data;
-    } catch(error) {
-    new Error('There is no movie');
-}
-};
-
-export async function getMoviesByName(query) {
-    const searchParams = new URLSearchParams({
-        api_key: API_KEY,
-        language: 'en-US',
-        page: 1,
-        include_adult: false,
-        query,
+export const getTradingMovies = () => {
+    return axios.get(`/trending/all/day?`)
+    .then(({data: {results}}) => 
+    results)
+    . catch (error => {
+       console.log(error.message);
     });
-    try {
-        const response = await axios.get(`${BASE_URL}/search/movie?${searchParams}`);
-        return response.data;
-    } catch (error) {
-        new Error(`There is no movie with name ${query}`);
-    }
 };
 
-export async function getMovieById(movieId) {
+export const getMoviesByName = query => {
+    return axios
+    .get(`/search/movie?query=${query}`)
+    .then(({data: {results}}) => 
+    results)
+    . catch (error => {
+       console.log(error.message);
+    });
+};
+
+export const getMovieById = movieId => {
     return axios
     .get(`/movie/${movieId}?`)
     .then(
