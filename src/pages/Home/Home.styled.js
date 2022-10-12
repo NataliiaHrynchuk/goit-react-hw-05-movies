23e0145b241f1753b47c0ec2fd0dea2ec4f9d581
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-12 19:11:22 +0300

    Create Home.styled.js

---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+text-align: center;
+color: #C71585;
+`;
+
+// export const StyledLink = styled(NavLink)`
+//     padding: 8px 16px;
+//     border-radius: 4px;
+//     /* text-decoration: none; */
+//     color: black;
+//     font-weight: 500;
+
+//     /* &.active {
+//     color: #C71585;
+//     } */
+
+//     :hover {
+//         text-decoration: underline;
+//     }
+// `;
