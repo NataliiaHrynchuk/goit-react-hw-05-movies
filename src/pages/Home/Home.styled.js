800bddaedb04aabadde10755bc5ebf2749d95fcc
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-14 22:11:51 +0300

    Update Home.styled.js

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -4,19 +4,3 @@ export const Title = styled.h1`
 text-align: center;
 color: #C71585;
 `;
-
-// export const StyledLink = styled(NavLink)`
-//     padding: 8px 16px;
-//     border-radius: 4px;
-//     /* text-decoration: none; */
-//     color: black;
-//     font-weight: 500;
-
-//     /* &.active {
-//     color: #C71585;
-//     } */
-
-//     :hover {
-//         text-decoration: underline;
-//     }
-// `;
