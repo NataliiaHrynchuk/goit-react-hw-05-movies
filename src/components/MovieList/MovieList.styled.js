79769ccd55bfac6ecebc5d8330f716e538be3895
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-14 22:11:32 +0300

    Update MovieList.styled.js

--- src/components/MovieList/MovieList.styled.js
+++ src/components/MovieList/MovieList.styled.js
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 export const Box = styled.ul`
     display: flex;
     flex-wrap: wrap;
+    justify-content: center;
     margin-left: auto;
     margin-right: auto;
-    width: 100%;
+    min-width: 800px;
+    max-width: 1200px;
     gap: 12px;
     padding: 0;
-    border-bottom: solid 1px grey;
-    box-shadow: 0px 3px 3px -3px rgba(0,0,0,0.55);
 `;
 
 export const MovieItem = styled.li`
@@ -25,7 +25,7 @@ export const MovieItem = styled.li`
     0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
     border-radius: 5px;
     text-decoration: none;
-    color: black;
+    text-align: center;
     transition-duration: 300ms;
     &:hover {
         cursor: pointer;
@@ -37,14 +37,13 @@ export const MovieItem = styled.li`
 export const StyledLink = styled(NavLink)`
     border-radius: 4px;
     text-decoration: none;
-    color: black;
+    color: #C71585;
     font-weight: 500;
 `;
 
 export const Poster = styled.img`
     padding: 0px;
     margin: 0px; 
-    /* height: 260px; */
     width: 100%;
     display: block;
     border-top-left-radius: 5px;
