f6351054b49031f7cd302e767aece08a49bd8c05
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-16 00:23:16 +0300

    Update MovieList.styled.js

--- src/components/MovieList/MovieList.styled.js
+++ src/components/MovieList/MovieList.styled.js
@@ -2,15 +2,21 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const Box = styled.ul`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-left: auto;
-    margin-right: auto;
-    min-width: 800px;
-    max-width: 1200px;
-    gap: 12px;
-    padding: 0;
+width: 100%;
+display: flex;
+flex-direction: column;
+gap: 12px;
+padding: 0;
+justify-content: center;
+    @media screen and (min-width: 768px){
+        max-width: 768px;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 60px;
+    }
+    @media screen and (min-width: 1200px) {
+        max-width: 1020px;
+    }
 `;
 
 export const MovieItem = styled.li`
@@ -18,8 +24,7 @@ export const MovieItem = styled.li`
     flex-direction: column;
     gap: 12px;
     width: 100%;
-    max-width: 200px;
-    height: 320 px;
+    margin-bottom: 12px;
     background: rgb(255, 255, 255);
     box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
     0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
@@ -32,7 +37,11 @@ export const MovieItem = styled.li`
         transform: scale(1.03);
         box-shadow: rgb(0 0 0 / 8%) 0px 10px 25px;
     }
-`
+    @media screen and (min-width: 768px) {
+        margin-bottom: 0;
+        width: 300px;
+    }
+`;
 
 export const StyledLink = styled(NavLink)`
     border-radius: 4px;
@@ -48,4 +57,11 @@ export const Poster = styled.img`
     display: block;
     border-top-left-radius: 5px;
     border-top-right-radius: 5px;
+`;
+
+export const MiddleTitle = styled.h3`
+    padding: 12px 0;
+    @media screen and (min-width: 1200px) {
+        font-size: 24px;
+    }
 `;
