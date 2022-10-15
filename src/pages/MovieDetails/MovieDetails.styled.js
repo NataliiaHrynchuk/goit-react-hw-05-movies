d45141fe5ee5e005c10f96b6a54b9f87bed02763
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-16 00:23:56 +0300

    Update MovieDetails.styled.js

--- src/pages/MovieDetails/MovieDetails.styled.js
+++ src/pages/MovieDetails/MovieDetails.styled.js
@@ -3,17 +3,80 @@ import { NavLink } from "react-router-dom";
 
 export const Box = styled.div`
     display: flex;
+    flex-direction: column; 
     gap: 12px;
-    padding: 12px;
+    padding: 16px;
     border-bottom: solid 1px grey;
     box-shadow: 0px 3px 3px -3px rgba(0,0,0,0.55);
+    @media screen and (min-width: 1200px) {
+        max-width: 1200px;
+    }
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column; 
+    gap: 12px;
+    padding: 12px 0;
+    border-bottom: solid 1px grey;
+    box-shadow: 0px 3px 3px -3px rgba(0,0,0,0.55);
+    @media screen and (min-width: 768px) {
+        max-width: 768px;
+        flex-direction: row;
+        gap: 12px;
+    }
+    @media screen and (min-width: 1200px) {
+        max-width: 1020px;
+    }
+`;
+
+export const Poster = styled.img`
+width: 100%;
+max-width: 400px;
+margin-left: auto;
+margin-right: auto;
 `;
 
 export const Wrap = styled.div`
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
     border-bottom: gray;
-    padding: 12px;
-    border-bottom: solid 1px grey;
-    box-shadow: 0px 3px 3px -3px rgba(0,0,0,0.55);
+    gap: 12px;
+    @media screen and (min-width: 768px) {
+        align-items: flex-start;
+    }
+
+    @media screen and (min-width: 1200px) {
+        padding: 16px;
+    }
+`;
+export const BigTitle = styled.h2`
+    @media screen and (min-width: 1200px) {
+        font-size: 30px;
+    }
+`;
+
+export const MiddleTitle = styled.h3`
+    @media screen and (min-width: 1200px) {
+        font-size: 24px;
+    }
+`;
+
+export const SmallTitle = styled.h4`
+    @media screen and (min-width: 1200px) {
+        font-size: 20px;
+    }
+`;
+
+export const Text = styled.p`
+    @media screen and (min-width: 768px) {
+        font-size: 20px;
+    }
+
+    @media screen and (min-width: 1200px) {
+        font-size: 24px;
+    }
 `;
 
 export const Link = styled(NavLink)`
@@ -30,10 +93,21 @@ export const Link = styled(NavLink)`
     :hover {
         text-decoration: underline;
     }
+
+    @media screen and (min-width: 1200px) {
+        font-size: 24px;
+    }
 `;
 
 export const DetailsList = styled.ul`
-display: flex;
-flex-direction: column; 
-gap: 20px;
+    display: flex;
+    flex-direction: column; 
+    gap: 20px;
+    list-style: none;
+    padding: 0;
+    text-align: center;
+
+    @media screen and (min-width: 768px) {
+        text-align: left;
+    }
 `;
