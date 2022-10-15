94bb406b0a142d9d69ee6567dee37be8535686d0
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-16 00:23:12 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,23 @@
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import * as SC from './MovieList.styled';
+import noResults from 'components/images/clipart2883707.png';
 
 export const MovieList = ({ movies }) => {
+    const [baseUrl] = useState('https://image.tmdb.org/t/p/w500');
     const location = useLocation();
+    
     return (
         <SC.Box>
-            {movies.map(({ id, title, name, poster}) =>
+            {movies.map(({ id, title, name, poster_path}) =>
                 <SC.MovieItem key = {id}>
             <SC.StyledLink to={`/movies/${id}`} state={{from:location}}>
-                <SC.Poster src={poster} alt={title || name}/>
-                <h3>{title || name}</h3>
+                <SC.Poster 
+                src={
+                    poster_path ? baseUrl + poster_path : noResults
+                    } 
+                    alt={title || name}/>
+                <SC.MiddleTitle>{title || name}</SC.MiddleTitle>
             </SC.StyledLink>
         </SC.MovieItem>)
 }
