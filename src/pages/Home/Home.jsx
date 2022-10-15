d5a7850c39d822999db19ebbd04419d97d540367
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-16 00:23:49 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,36 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { MovieList } from 'components/MovieList/MovieList';
 import * as API from 'services/api';
 import * as SC from './Home.styled';
-const IMG_URL = 'https://image.tmdb.org/t/p/w200';
-
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
+    const location = useLocation();
     
     useEffect(() => {
         API
             .getTradingMovies()
-            .then((results) => setMovies(results.map(
-                    ({ id,
-                        title,
-                        name,
-                        poster_path: poster,
-                    }) => {
-                        return {
-                            id,
-                            title,
-                            name,
-                            poster: IMG_URL + poster,
-                        }}
-                )));
+            .then(setMovies)
     }, []);
 
     return (
         <main>
             <SC.Title>Trending today</SC.Title>
             {movies &&
-                <MovieList movies={movies}/>
+                <MovieList movies={movies} location={location}/>
             }
         </main>
     )
