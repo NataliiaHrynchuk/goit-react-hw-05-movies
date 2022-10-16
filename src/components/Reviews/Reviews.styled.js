fcb0cd24ae9a489a4b0edd7be0c409daf09a7e2f
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-10-16 22:27:23 +0300

    Create Reviews.styled.js

---
+++ src/components/Reviews/Reviews.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ReviewsList = styled.ul`
+    list-style: none;
+    padding-left: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`;
