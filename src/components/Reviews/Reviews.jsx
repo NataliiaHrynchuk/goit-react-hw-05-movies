f0d7817b91d16fdf4e66e616db634ec2a6baab01
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-16 16:00:24 +0300

    Create Reviews.jsx

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,38 @@
+import * as API from '../../services/api';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
+const Reviews = () => {
+    const { movieId } = useParams();
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        API
+            .getReviews(movieId)
+            .then(results => 
+                setReviews(results.map(({id, author, content}) => ({
+                        id,
+                        author, 
+                        content,
+                    })))); 
+    }, [movieId]);
+
+    return (
+        <div>
+            {reviews.length > 0 ? (
+                <ul>
+                {reviews.map(({id, author, content}) => (
+                    <li key={id}>
+                        <h3>{author}</h3>
+                        <p>{content}</p>
+                    </li>
+                ))}
+                </ul>
+            ) : (
+                <p>We don`t have any reviews for this movie</p>
+            )}
+        </div>
+    )
+};
+
+export default Reviews;
