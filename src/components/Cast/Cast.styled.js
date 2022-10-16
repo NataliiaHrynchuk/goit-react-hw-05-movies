5db8d31e536a621d86ed099fa0caaca7320f3ad1
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-10-16 16:00:07 +0300

    Create Cast.styled.js

---
+++ src/components/Cast/Cast.styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const CastList = styled.ul`
+list-style: none;
+`;
+
+export const CastItem = styled.li`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    width: 100%;
+    margin-bottom: 12px;
+    background: rgb(255, 255, 255);
+    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+    border-radius: 5px;
+`;
+
+export const Name = styled.p`
+
+`;
+
+export const Character = styled.p`
+
+`;
+
