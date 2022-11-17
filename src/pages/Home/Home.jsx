5c6d3cf38d730d743c79e57b4d1aa0366e144721
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-11-17 10:05:05 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { MovieList } from 'components/MovieList/MovieList';
+import { NoResults } from 'components/NoResults/NoResults';
+import noResults from 'components/images/clipart2883707.png';
 import * as API from 'services/api';
 import * as SC from './Home.styled';
 
 const Home = () => {
+    const [isLoadingMovies, setIsLoadingMovies] = useState(false);
     const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
     const location = useLocation();
     
     useEffect(() => {
-        API
-            .getTradingMovies()
-            .then(setMovies)
+        const fetchTradingMovies = async () => {
+            try {
+                setIsLoadingMovies(true);
+                const movies = await API.getTradingMovies();
+                setMovies(movies);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoadingMovies(false);
+            }
+        };
+        fetchTradingMovies();
     }, []);
-
-    // console.log(location);
 
     return (
         <main>
             <SC.Title>Trending today</SC.Title>
-            {movies &&
+            {error && (
+                <>
+                    <NoResults imageUrl={noResults} location={location} />
+                    <p>{error}</p>
+                </>
+            )
+                }
+            {isLoadingMovies && <p>Loading...</p>}
+            {movies && !isLoadingMovies &&
                 <MovieList movies={movies} location={location}/>
             }
         </main>
